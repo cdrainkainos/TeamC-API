0109f8d7abd421fcb8aac3747ecdac2119e2f9f4
MartaMartaZielonka <marta.zielonka@kainos.com>
2022-11-24 11:21:24 +0100

    SQL script to populate data added.

---
+++ src/databaseScripts/InsertData.sql
@@ -0,0 +1,42 @@
+
+USE  AgileSprints_SamB;
+
+INSERT INTO Capabilities(CapabilityName)
+	VALUES("Engineering"),
+	("Data & AI");
+
+INSERT INTO Bands(BandLevel, BandName)
+	VALUES(1, "Executive"),
+    (1, "Leader"),
+    (1, "Principal"),
+    (2, "Manager"),
+	(3, "Consultant"),
+	(4, "Senior Associate"),
+	(5, "Associate"),
+	(6, "Apprentice"),
+	(6, "Trainee");
+
+INSERT INTO Trainings(TrainingName, IsObligatory)
+	VALUES("GDPR & Handling Sensitive Data", 1),
+	("Inclusion, Diversity, Equality & Equity", 1),
+	("Life as a PLC", 1),
+	("Security Awareness", 1),
+	("A Guide to Mentoring", 0),
+	("Coaching for Skills - People Managers", 0),
+	("Business Development - Stakeholder Management and Negotiation Skills", 0),
+	("Coaching Lessons", 0),
+	("Coaching Videos", 0),
+	("Hybrid Working - A guide to working remotely", 0),
+	("Mindset", 0),
+	("Mental Health", 0),
+	("Leadership Competency Framework - How To…", 0),
+	("Pre-Course Learning: Having Courageous Conversations", 0),
+	("Steps for Success", 0),
+	("The People Process Series", 0),
+	("Xpression - Becoming an Ally", 0),
+	("Wellbeing Guides", 0),
+	("Unconscious Bias", 0),
+	("Workday - Bitesize 101", 0);
+
+
+

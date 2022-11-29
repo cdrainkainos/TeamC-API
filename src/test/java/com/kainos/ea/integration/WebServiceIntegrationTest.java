5703a66c8521f828cb17a83df53fac1483eacb1e
Samuel Bannister <sam.bannister@kainos.com>
2022-11-29 15:15:08 +0000

    Test added to test for the band level returned

--- src/test/java/com/kainos/ea/integration/WebServiceIntegrationTest.java
+++ src/test/java/com/kainos/ea/integration/WebServiceIntegrationTest.java
@@ -1,11 +1,15 @@
 package com.kainos.ea.integration;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kainos.ea.trueApplication;
 import com.kainos.ea.trueConfiguration;
 import com.kainos.ea.model.JobRole;
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -21,13 +25,26 @@ public class WebServiceIntegrationTest {
             new ResourceConfigurationSourceProvider()
     );
 
-
     @Test
-    void getEmployees_shouldReturnListOfEmployees() {
+    void getEmployees_shouldReturnListOfRoles() {
         List<JobRole> response = APP.client().target("http://localhost:8080/api/job-roles")
                 .request()
                 .get(List.class);
         assertTrue(response.size() > 0);
+    }
+
+    @Test
+    void getJobRoles_shouldReturnListOfJobRoles_withValidBandName() {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode response = APP.client().target("http://localhost:8080/api/job-roles")
+                .request()
+                .get(JsonNode.class);
+        List<JobRole> JobRoleList = mapper.convertValue(
+                response, new TypeReference<List<JobRole>>(){}
+        );
+
+        Assertions.assertEquals(JobRoleList.get(0).getBandName(), "Executive");
+
     }
 
 }

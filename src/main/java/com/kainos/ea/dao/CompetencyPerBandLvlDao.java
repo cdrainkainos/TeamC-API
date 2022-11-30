c2c95ea4e8771985892cc74b2cac82756ccf27fc
Tomasz Łaski <tomasz.laski0001@gmail.com>
2022-11-30 11:19:50 +0100

    added dao without query file and custom exception handler

--- src/main/java/com/kainos/ea/dao/CompetencyPerBandLvlDao.java
+++ src/main/java/com/kainos/ea/dao/CompetencyPerBandLvlDao.java
@@ -1,11 +1,39 @@
 package com.kainos.ea.dao;
 
+import com.kainos.ea.exception.CompetencyPerBandLvlNotExistException;
 import com.kainos.ea.model.CompetencyPerBandLvl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CompetencyPerBandLvlDao {
     public CompetencyPerBandLvlDao(){
     }
+    public List<CompetencyPerBandLvl> getAllCompetencyPerBandLvl(Connection c, int role_id) throws SQLException, CompetencyPerBandLvlNotExistException {
 
+        try{
+            Statement st = c.createStatement();
+            ResultSet rs = st.executeQuery("" +role_id );
+            List<CompetencyPerBandLvl> competencyPerBandLvl = new ArrayList<>();
+
+            while (rs.next()) {
+                CompetencyPerBandLvl jobSpecification = new CompetencyPerBandLvl(
+                        rs.getInt("band_level"),
+                        rs.getString("band_name"),
+                        rs.getString("competency_name"));
+                competencyPerBandLvl.add(jobSpecification);
+            }
+
+            if(competencyPerBandLvl.isEmpty()){
+                throw new CompetencyPerBandLvlNotExistException("Select record that is not empty");
+            } else return competencyPerBandLvl;
+
+        } catch (SQLException e) {
+            throw new SQLException ("Select query for job specifications is incorrect");
+        }
+    }
 }
---
+++ src/main/java/com/kainos/ea/exception/CompetencyPerBandLvlNotExistException.java
@@ -0,0 +1,7 @@
+package com.kainos.ea.exception;
+
+public class CompetencyPerBandLvlNotExistException  extends Exception{
+    public CompetencyPerBandLvlNotExistException(String message) {
+        super(message);
+    }
+}
